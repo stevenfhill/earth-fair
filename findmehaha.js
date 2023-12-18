1924a5b921c7930611479babd8b2e47723feee76
stevenfhill <92600162+stevenfhill@users.noreply.github.com>
2023-12-18 04:27:46 -0500

    Update findmehaha.js

--- findmehaha.js
+++ findmehaha.js
@@ -1,27 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Initialize the map
-    var map = L.map('map').setView([0, 0], 2); // Default view
+// findmehaha.js
 
-    // Add a basemap (you can replace this with your preferred basemap)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+// Function to handle location found
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
 
-    // Add geolocation control to the map
-    L.control.locate().addTo(map);
+    // Add a marker at the found location
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-    // Listen for locationfound event
-    map.on('locationfound', onLocationFound);
+    // Add a circle to represent the accuracy of the location
+    L.circle(e.latlng, radius).addTo(map);
+}
 
-    // Handle location found
-    function onLocationFound(e) {
-        var radius = e.accuracy / 2;
+// Function to handle location error
+function onLocationError(e) {
+    alert(e.message);
+}
 
-        // Add a marker at the found location
-        L.marker(e.latlng).addTo(map)
-            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+// Initialize the map
+var map = L.map('map').setView([0, 0], 2); // Default view
 
-        // Add a circle to represent the accuracy of the location
-        L.circle(e.latlng, radius).addTo(map);
-    }
-});
+// Add a basemap (you can replace this with your preferred basemap)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Add geolocation control to the map
+L.control.locate({
+    setView: 'always', // or use 'untilPan', 'untilPanOrZoom', 'always'
+    watch: true // enable continuous watching
+}).addTo(map);
+
+// Listen for locationfound event
+map.on('locationfound', onLocationFound);
+
+// Listen for locationerror event
+map.on('locationerror', onLocationError);
